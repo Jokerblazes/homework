47047a06fa503b7509bd8530ca2733c658110b91
Jokerblazes <zcd123@qq.com>
2018-04-05 19:47:15 +0800

    完成section3的PracticeD（统计出collection1中相同的元素的个数，有过有-就把-右边的数字也计算入个数，形成collection3，collection3是一个map其中元素key是collection1中的元素value是该元素在collection1中出现次数，然后选出collection3中的元素的key属性跟collection2中value属性对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合）

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,25 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> result = new HashMap<>();
+        final char zero = '0';
+        for (String s : collectionA) {
+            if (s.length() != 1) {
+                char c = s.charAt(0);
+                char number = s.charAt(2);
+                result.merge(String.valueOf(c), number - zero, (a, b) -> a + b);
+            } else {
+                result.merge(s,1,(a,b) -> a + b);
+            }
+        }
 
-        return null;
+        PracticeB practiceB = new PracticeB();
+        return practiceB.createUpdatedCollection(result,object);
     }
 }
