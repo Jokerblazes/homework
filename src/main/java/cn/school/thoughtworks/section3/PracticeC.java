6679706b314bd735161bc692954c49391f9ed6c4
Jokerblazes <zcd123@qq.com>
2018-04-05 19:34:03 +0800

    完成section3的PracticeC（统计出collection1中相同的元素的个数，形成collection3，collection3是一个map其中元素key是collection1中的元素value是该元素在collection1中出现次数，然后选出collection3跟collection2中value属性对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合）

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,18 @@
 package cn.school.thoughtworks.section3;
 
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> coolectionC = new HashMap<>();
+        for (String s : collectionA) {
+            coolectionC.merge(s,1,(a,b) -> a + b);
+        }
+        PracticeB practiceB = new PracticeB();
+        return practiceB.createUpdatedCollection(coolectionC,object);
     }
 }
