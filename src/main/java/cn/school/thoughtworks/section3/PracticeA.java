bafa1d10a9532938ffa7e4c84366f079fff92f36
Jokerblazes <zcd123@qq.com>
2018-04-05 19:13:10 +0800

    完成section3的PracticeA任务（选出1集合中元素的key的值跟2对象中value属性中的元素相同的元素,把他们的count-1，输出减过之后的新集合）

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,13 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for (Map.Entry<String, List<String>> listEntry:
+        object.entrySet()){
+            for (String s : listEntry.getValue()) {
+                if (collectionA.get(s) != null)
+                    collectionA.merge(s,-1,(a,b) -> a + b);
+            }
+        }
+        return collectionA;
     }
 }
