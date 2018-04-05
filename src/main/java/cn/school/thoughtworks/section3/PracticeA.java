01e6ecda5c1c4ea0efdef885d3d5ba9a760b37ae
Jokerblazes <zcd123@qq.com>
2018-04-05 19:19:38 +0800

    去除上版本的存在的魔术数字。

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,11 +6,13 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        final int reduceNumber = -1;
         for (Map.Entry<String, List<String>> listEntry:
         object.entrySet()){
             for (String s : listEntry.getValue()) {
-                if (collectionA.get(s) != null)
-                    collectionA.merge(s,-1,(a,b) -> a + b);
+                if (collectionA.get(s) != null) {
+                    collectionA.merge(s, reduceNumber,(a, b) -> a + b);
+                }
             }
         }
         return collectionA;
