dd0f69a12f65f7df27260803c9d8a22559c24f0f
Jokerblazes <zcd123@qq.com>
2018-04-05 19:24:39 +0800

    完成section3的PracticeB（选出collection1中key跟collection2中value对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合）

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,17 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        final int base = 3;
+        for (Map.Entry<String, List<String>> listEntry:
+                object.entrySet()){
+            for (String s : listEntry.getValue()) {
+                Integer number = collectionA.get(s);
+                if (number != null) {
+                    int reduceNum = - number / base;
+                    collectionA.merge(s,reduceNum,(a,b) -> a + b);
+                }
+            }
+        }
+        return collectionA;
     }
 }
