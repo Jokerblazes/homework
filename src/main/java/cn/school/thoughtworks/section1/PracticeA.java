bc39e3660cfc0ca2eb31df5011e4fb859d32e584
Jokerblazes <zcd123@qq.com>
2018-04-05 16:29:17 +0800

    完成PracticeA任务（选出集合1和集合2中相同的元素）

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,35 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        /*
+        * 集合中可能会出现重复的字母
+        * 使用map
+        * key记录字母
+        * value记录次数
+        * */
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : collection2) {
+            map.merge(s, 1, (a, b) -> a + b);
+        }
+
+        for (String s : collection1) {
+            Integer integer = map.get(s);
+            if (integer != null && integer > 0) {
+                result.add(s);
+                if (integer == 1)
+                    map.remove(s);
+                else
+                    map.put(s,integer - 1);
+            }
+        }
+        return result;
     }
 }
