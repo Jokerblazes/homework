74078b1d017b3b96dc5f1d80f863b988c3138e89
Jokerblazes <zcd123@qq.com>
2018-04-05 18:17:04 +0800

    完成section1的PracticeC任务（选出集合1和2对象中value属性中元素相同的元素）

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,30 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (Map.Entry<String,List<String>> entry:
+        collection2.entrySet()) {
+            for (String s : entry.getValue()) {
+                map.merge(s, 1, (a, b) -> a + b);
+            }
+        }
+
+        for (String s : collection1) {
+            Integer integer = map.get(s);
+            if (integer != null && integer > 0) {
+                result.add(s);
+                if (integer == 1)
+                    map.remove(s);
+                else
+                    map.put(s,integer - 1);
+            }
+        }
+        return result;
     }
 }
